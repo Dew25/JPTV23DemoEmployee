82b2c371b951f2d2ba577aaebf96d8cf3f049869
TeacherIVKHK <teacherivkhk@ivkhk.ee>
2024-09-23 17:07:19 +0300

    Реализовали редактирование

--- src/main/java/ee/ivkhkdev/tools/EmployeeInputOutput.java
+++ src/main/java/ee/ivkhkdev/tools/EmployeeInputOutput.java
@@ -1,7 +1,9 @@
 package ee.ivkhkdev.tools;
 
+import ee.ivkhkdev.App;
 import ee.ivkhkdev.model.Employee;
 import ee.ivkhkdev.model.EmployeeRecord;
+import ee.ivkhkdev.service.EmployeeService;
 
 import java.util.Scanner;
 
@@ -30,11 +32,11 @@ public class EmployeeInputOutput {
         System.out.print("Зарплата: ");
         String salary = scanner.nextLine();
         System.out.print("День рождения: ");
-        int birthDay = this.employeeInput(scanner);
+        int birthDay = this.inputNumber(scanner);
         System.out.print("Месяц рождения: ");
-        int birthMonth = this.employeeInput(scanner);
+        int birthMonth = this.inputNumber(scanner);
         System.out.print("Год рождения: ");
-        int birthYear = this.employeeInput(scanner);
+        int birthYear = this.inputNumber(scanner);
         System.out.print("Телефон: ");
         String phone = scanner.nextLine();
         System.out.println("Адрес");
@@ -51,7 +53,7 @@ public class EmployeeInputOutput {
                 birthMonth, birthYear, phone, city, street, house, room
         );
     }
-    public int employeeInput(Scanner scanner) {
+    public int inputNumber(Scanner scanner) {
         int number;
         do {
             try {
@@ -66,4 +68,97 @@ public class EmployeeInputOutput {
     }
 
 
+    public Employee editEmployee(Scanner scanner) {
+        System.out.println("Введите имя работника");
+        String firstNameFind = scanner.nextLine();
+        System.out.print("Введите фамилию работника: ");
+        String lastNameFind = scanner.nextLine();
+        EmployeeService employeeService = new EmployeeService();
+        Employee employee = employeeService.findEmployee(firstNameFind,lastNameFind, App.employees);
+
+        System.out.print("Имя: "+employee.getPerson().getFirstName());
+        System.out.println("Заменить? 1-да/0-нет");
+        if(this.inputNumber(scanner)==1){
+            System.out.println("Введите новое значение: ");
+            employee.getPerson().setFirstName(scanner.nextLine());
+        }
+        String firstName = scanner.nextLine();
+        System.out.print("Фамилия: "+employee.getPerson().getLastName());
+        System.out.println("Заменить? 1-да/0-нет");
+        if(this.inputNumber(scanner)==1){
+            System.out.println("Введите новое значение: ");
+            employee.getPerson().setLastName(scanner.nextLine());
+        }
+
+        System.out.print("Должность: "+employee.getPosition());
+        System.out.println("Заменить? 1-да/0-нет");
+        if(this.inputNumber(scanner)==1){
+            System.out.println("Введите новое значение: ");
+            employee.setPosition(scanner.nextLine());
+        }
+
+        System.out.print("Зарплата: "+employee.getSalary());
+        System.out.println("Заменить? 1-да/0-нет");
+        if(this.inputNumber(scanner)==1){
+            System.out.println("Введите новое значение: ");
+            employee.setSalary(scanner.nextLine());
+        }
+        System.out.print("День рождения: "+employee.getPerson().getBirthDay());
+        System.out.println("Заменить? 1-да/0-нет");
+        if(this.inputNumber(scanner)==1){
+            System.out.println("Введите новое значение: ");
+            employee.getPerson().setBirthDay(this.inputNumber(scanner));
+        }
+
+        System.out.print("Месяц рождения: "+employee.getPerson().getBirthMonth());
+        System.out.println("Заменить? 1-да/0-нет");
+        if(this.inputNumber(scanner)==1){
+            System.out.println("Введите новое значение: ");
+            employee.getPerson().setBirthMonth(this.inputNumber(scanner));
+        }
+
+        System.out.print("Год рождения: "+employee.getPerson().getBirthYear());
+        System.out.println("Заменить? 1-да/0-нет");
+        if(this.inputNumber(scanner)==1){
+            System.out.println("Введите новое значение: ");
+            employee.getPerson().setBirthYear(this.inputNumber(scanner));
+        }
+
+        System.out.print("Телефон: "+employee.getPerson().getPhone());
+        System.out.println("Заменить? 1-да/0-нет");
+        if(this.inputNumber(scanner)==1){
+            System.out.println("Введите новое значение: ");
+            employee.getPerson().setPhone(scanner.nextLine());
+        }
+        System.out.println("Адрес");
+        System.out.print("Город: "+employee.getPerson().getAddress().getCity());
+        System.out.println("Заменить? 1-да/0-нет");
+        if(this.inputNumber(scanner)==1){
+            System.out.println("Введите новое значение: ");
+            employee.getPerson().getAddress().setCity(scanner.nextLine());
+        }
+
+        System.out.print("Улица: "+employee.getPerson().getAddress().getStreet());
+        System.out.println("Заменить? 1-да/0-нет");
+        if(this.inputNumber(scanner)==1){
+            System.out.println("Введите новое значение: ");
+            employee.getPerson().getAddress().setStreet(scanner.nextLine());
+        }
+
+        System.out.print("Дом: "+employee.getPerson().getAddress().getHouse());
+        System.out.println("Заменить? 1-да/0-нет");
+        if(this.inputNumber(scanner)==1){
+            System.out.println("Введите новое значение: ");
+            employee.getPerson().getAddress().setHouse(scanner.nextLine());
+        }
+
+        System.out.print("Квартира: "+employee.getPerson().getAddress().getRoom());
+        System.out.println("Заменить? 1-да/0-нет");
+        if(this.inputNumber(scanner)==1){
+            System.out.println("Введите новое значение: ");
+            employee.getPerson().getAddress().setRoom(scanner.nextLine());
+        }
+
+        return employee;
+    }
 }
